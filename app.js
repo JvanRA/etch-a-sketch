5d87e62e8f337797e511aa12a3c0a246a52d0fb9
JvanRA <anorajivan@gmail.com>
2022-04-03 15:14:16 +0800

    Removed RGB mode and improved UI

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const grid = document.getElementById('grid');
-const resetBtn = document.getElementById('resetBtn');
+const resetBtn = document.querySelector('.button-52');
 let inputRange = document.querySelector('input');
 let gridValue = document.querySelector('.grid-value');
-const rainbowBtn = document.querySelector('.rainbowBtn');
 let size = 16;
 
 createDiv();
@@ -13,27 +12,10 @@ function createDiv(divAmount) {
     for (let i=0; i<size*size; i++) {
         const div = document.createElement('div');
         div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = 'black';
+            div.style.backgroundColor = 'rgb(35,35,35)';
         })
         grid.append(div);
     }
-};
-
-function rainbowMode(divAmount) {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    for (let i=0; i<size*size; i++) {
-        const div = document.createElement('div');
-        div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = `rgb(${randNum255()},${randNum255()},${randNum255()})`;
-        })
-        grid.append(div);
-    }
-};
-
-function randNum255() {
-    let randNum = Math.floor(Math.random()*256);
-    return randNum;
 };
 
 function reset() {
@@ -47,13 +29,6 @@ inputRange.addEventListener('input', function(e) {
     size = e.target.value;
     gridValue.textContent = `${size}x${size}`;
     reset();
-});
-
-rainbowBtn.addEventListener('click', () => {
-    while (grid.firstChild) {
-        grid.removeChild(grid.lastChild);
-    }
-    rainbowMode();
 });
 
 resetBtn.addEventListener('click', reset);
